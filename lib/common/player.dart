3e88b6963371293fb4cff915ef6f57180fa50280
Mauro Vanetti <posta@maurovanetti.info>
2023-01-17 11:38:31 +0100

    Team building

---
+++ lib/common/player.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+enum Gender {
+  m,
+  f,
+}
+
+class Player {
+  int id;
+  String name;
+  Gender gender;
+
+  Player(this.id, this.name, this.gender);
+
+  static const _icons = [
+    '🎹',
+    '🎸',
+    '🎷',
+    '🎤',
+    '🎻',
+    '🥁',
+    '🎺',
+  ];
+  String get icon => _icons[id % _icons.length];
+
+  String get genderSymbol {
+    switch (gender) {
+      case Gender.f:
+        return "♀";
+      case Gender.m:
+        return "♂";
+    }
+  }
+
+  static final _foregroundColors = [
+    Colors.purple.shade900,
+    Colors.pink.shade900,
+    Colors.brown.shade900,
+    Colors.indigo.shade900,
+    Colors.green.shade900,
+    Colors.black87,
+    Colors.deepOrange.shade900,
+  ];
+  static final _backgroundColors = [
+    Colors.purpleAccent.shade100,
+    Colors.pinkAccent.shade100,
+    Colors.brown.shade300,
+    Colors.indigoAccent.shade100,
+    Colors.greenAccent.shade100,
+    Colors.grey.shade100,
+    Colors.deepOrangeAccent.shade100,
+  ];
+  Color get background => _backgroundColors[id % _backgroundColors.length];
+  Color get foreground => _foregroundColors[id % _foregroundColors.length];
+}
--- lib/team_page.dart
+++ lib/team_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'common/custom_fab.dart';
 import 'common/navigation.dart';
+import 'common/player.dart';
 import 'pick_page.dart';
 
 class TeamPage extends StatefulWidget {
@@ -12,24 +13,71 @@ class TeamPage extends StatefulWidget {
 }
 
 class _TeamPageState extends State<TeamPage> {
+  List<NewPlayer> _players = [
+    NewPlayer(Player(0, 'FEDE', Gender.f)),
+    NewPlayer(Player(1, 'ROBY', Gender.m)),
+    NewPlayer(Player(2, 'NADIA', Gender.f)),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Registra i partecipanti'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text("Roba da fare"),
-          ],
-        ),
+      body: ListView(
+        padding: const EdgeInsets.all(30),
+        children: _players,
       ),
       floatingActionButton: CustomFloatingActionButton(
         onPressed: Navigation.replaceAll(context, () => const PickPage()).go,
         tooltip: 'Pronti',
         icon: Icons.check_circle_rounded,
+      ),
+    );
+  }
+}
+
+class NewPlayer extends StatefulWidget {
+  final Player initialPlayerData;
+  const NewPlayer(this.initialPlayerData, {super.key});
+
+  @override
+  State<NewPlayer> createState() => _NewPlayerState();
+}
+
+class _NewPlayerState extends State<NewPlayer> {
+  late Player player;
+
+  @override
+  void initState() {
+    player = widget.initialPlayerData;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final buttonStyle = ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(player.background));
+    final textColor = player.foreground;
+    final textStyle = theme.textTheme.headlineLarge
+        ?.copyWith(color: textColor, fontWeight: FontWeight.bold);
+
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: buttonStyle,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(player.icon, style: Theme.of(context).textTheme.headlineSmall),
+            Text(player.name.toUpperCase(), style: textStyle),
+            Text(player.genderSymbol, style: textStyle),
+          ],
+        ),
       ),
     );
   }
