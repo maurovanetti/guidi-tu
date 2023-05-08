6d92ef079c6ccb7d6622a068a4751c8fb3b45ff4
Mauro Vanetti <posta@maurovanetti.info>
2023-05-08 10:00:43 +0200

    Steady Hand: moving platform

--- lib/games/steady_hand/steady_hand_platform.dart
+++ lib/games/steady_hand/steady_hand_platform.dart
@@ -1,10 +1,16 @@
 import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
 
 import '/games/flame/custom_sprite_component.dart';
 
 class SteadyHandPlatform extends CustomSpriteComponent {
   final double radius;
+
+  static const shortStepDurationInSeconds = 3.0;
+
+  get shortStep => LinearEffectController(shortStepDurationInSeconds);
+  get longStep => LinearEffectController(shortStepDurationInSeconds * 2);
 
   SteadyHandPlatform(Vector2 position, {required this.radius})
       : super(
@@ -19,6 +25,37 @@ class SteadyHandPlatform extends CustomSpriteComponent {
 
   bool isUnder(Body item) {
     final distance = position.distanceTo(item.position);
-    return distance < radius;
+    // This works because the platform is a circle
+    return distance < radius * scale.x;
+  }
+
+  @override
+  Future<void> onLoad() {
+    var waitThenShrink = SequenceEffect(
+      [
+        MoveEffect.by(Vector2.zero(), longStep), // wait
+        ScaleEffect.by(Vector2.all(0.9), longStep), // shrink
+      ],
+      onComplete: _cycle,
+    );
+    add(waitThenShrink);
+    return super.onLoad();
+  }
+
+  void _cycle() {
+    var delta = (radius * 2) - scaledSize.x;
+    var sequence = SequenceEffect(
+      [
+        MoveEffect.by(Vector2.all(-delta / 2), shortStep), // to NW
+        MoveEffect.by(Vector2(delta, 0), shortStep), // to NE
+        MoveEffect.by(Vector2(0, delta), shortStep), // to SE
+        MoveEffect.by(Vector2(-delta, 0), shortStep), // to SW
+        MoveEffect.by(Vector2(0, -delta), shortStep), // to NW
+        MoveEffect.by(Vector2.all(delta / 2), shortStep), // to center
+        ScaleEffect.by(Vector2.all(0.8), longStep), // shrink more
+      ],
+      onComplete: _cycle,
+    );
+    add(sequence);
   }
 }
