eb6caa6d348d599313e3df87a1d53e4a0e727871
Mauro Vanetti <posta@maurovanetti.info>
2024-01-30 10:23:24 +0100

    DCM

--- lib/screens/pick_screen.dart
+++ lib/screens/pick_screen.dart
@@ -73,7 +73,7 @@ class _PickScreenState extends TrackedState<PickScreen>
 
   void _startGame(GameCard game) {
     resetTurn(rounds: game.rounds);
-    nextTurn();
+    var _ = nextTurn();
     if (mounted) {
       Navigation.push(context, () => game.gameStart).go();
     }
